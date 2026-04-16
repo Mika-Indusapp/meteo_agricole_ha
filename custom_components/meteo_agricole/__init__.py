1f144decec3a62758f2e67f6ff80b5911c81d7c1
Mika-Indusapp <mickael.samson@indusapp.fr>
2026-04-16 21:01:31 +0200

    Update __init__.py

--- custom_components/meteo_agricole/__init__.py
+++ custom_components/meteo_agricole/__init__.py
@@ -7,11 +7,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Configure La Météo Agricole à partir d'une entrée de configuration."""
     hass.data.setdefault(DOMAIN, {})
 
-    # On demande à HA de charger le fichier sensor.py
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    # On demande à HA de charger le fichier weather.py
+    await hass.config_entries.async_forward_entry_setups(entry, ["weather"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Décharge une entrée de configuration (quand on supprime l'intégration)."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["weather"])
     return unload_ok
